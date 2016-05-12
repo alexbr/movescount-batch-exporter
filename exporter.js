b5de930247bb1005ae1129fa7485978ec5a39615
alexbr <alexbr@gmail.com>
2016-05-11 19:30:12 -0700

    Remove whitespace, scope code.

--- exporter.js
+++ exporter.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name       Movescount Batch Exporter
 // @namespace  http://alexbr.com
-// @version    0.1
+// @version    0.2
 // @description  Batch export moves from Movescount. Based on http://userscripts-mirror.org/scripts/show/155662
 // @match      http://*.movescount.com/summary
 // @include    htt*://*.movescount.com/summary
-// @copyright  2015, AlexR
 // @require http://code.jquery.com/jquery-2.1.4.min.js
 // @require https://raw.github.com/lodash/lodash/3.10.0/lodash.min.js
 // ==/UserScript==
+(function() {
+"use strict";
+
 function exportMoves(format) {
     var moveIds = [];
     var wins = [];
@@ -19,17 +21,17 @@ function exportMoves(format) {
     if (confirm("This will export " + moveIds.length + " in format: " + format + ". Press Ok to export or Cancel to abort.")) {
         _.each(moveIds, function(moveId) {
             var urlstring = 'http://www.movescount.com/move/export?id=' + moveId + '&format=';
-            if (format === 'all') {                              
+            if (format === 'all') {
                 _.forOwn(formats, function(f) {
                     if (f !== 'all') {
-                        try {                    
+                        try {
                             wins.push(window.open(urlstring + f));
                         } catch (err) {
                             window.alert("Error: " + err.toString );
                         }
                     }
                 });
-            } else {     
+            } else {
                 try {
                     wins.push(window.open(urlstring + format));
                 } catch (err) {
@@ -51,19 +53,21 @@ var formats = {
     'All Formats': 'all',
 };
 
-setInterval(function() {    
+setInterval(function() {
     var toolsItem = $('a[data-action="toggleShowPastPlannedMoves"]');
+    console.warn(toolsItem);
     var sentinel = toolsItem.closest('ul').children('li.batchExporter');
     if (!sentinel || sentinel.length === 0) {
-        _.forOwn(formats, function(format, name) {    
+        _.forOwn(formats, function(format, name) {
             var li = $('<li class="batchExporter"></li>');
             var link = $('<a style="text-align: left;">Export selected as ' + name + '.</a>');
             link.click(function() {
                 exportMoves(format);
-                return false; 
+                return false;
             });
             li.append(link);
             toolsItem.closest('li').before(li);
         });
     }
 }, 1000);
+})();
