2f0eb140b82277fde67f45ac153b7141fe174bfa
Alex Rodriguez <alexbr@gmail.com>
2019-01-15 11:29:37 -0800

    Refresh the script and add the batch export links as a menu.

--- exporter.js
+++ exporter.js
@@ -1,73 +1,91 @@
 // ==UserScript==
 // @name       Movescount Batch Exporter
 // @namespace  http://alexbr.com
-// @version    0.2
+// @version    0.3
 // @description  Batch export moves from Movescount. Based on http://userscripts-mirror.org/scripts/show/155662
 // @match      http://*.movescount.com/summary
 // @include    htt*://*.movescount.com/summary
 // @require http://code.jquery.com/jquery-2.1.4.min.js
-// @require https://raw.github.com/lodash/lodash/3.10.0/lodash.min.js
+// @require https://cdn.jsdelivr.net/npm/lodash@4.17.11/lodash.min.js
 // ==/UserScript==
 (function() {
-"use strict";
+  'use strict';
+  /* global _ */
+  /* global $ */
 
-function exportMoves(format) {
-    var moveIds = [];
-    var wins = [];
+  function exportMoves(format) {
+    const moveIds = [];
+    const wins = [];
+
     $('a[data-id^="move-"] i.active').each(function() {
-        moveIds.push($(this).parent().attr('data-id').substring(5));
+      moveIds.push($(this).parent().attr('data-id').substring(5));
     });
 
-    if (confirm("This will export " + moveIds.length + " in format: " + format + ". Press Ok to export or Cancel to abort.")) {
-        _.each(moveIds, function(moveId) {
-            var urlstring = 'http://www.movescount.com/move/export?id=' + moveId + '&format=';
-            if (format === 'all') {
-                _.forOwn(formats, function(f) {
-                    if (f !== 'all') {
-                        try {
-                            wins.push(window.open(urlstring + f));
-                        } catch (err) {
-                            window.alert("Error: " + err.toString );
-                        }
-                    }
-                });
-            } else {
-                try {
-                    wins.push(window.open(urlstring + format));
-                } catch (err) {
-                    window.alert("Error: " + err.toString);
-                }
+    if (moveIds.length &&
+        confirm(`This will export ${moveIds.length} in format ${format}. Press Ok to export or Cancel to abort.`)) {
+      _.each(moveIds, function(moveId) {
+        const urlstring = `http://www.movescount.com/move/export?id=${moveId}&format=`;
+        if (format === 'all') {
+          _.forOwn(formats, function(f) {
+            if (f !== 'all') {
+              try {
+                wins.push(window.open(urlstring + f));
+              } catch (err) {
+                window.alert(`Error: ${err}`);
+              }
             }
-        });
+          });
+        } else {
+          try {
+            wins.push(window.open(urlstring + format));
+          } catch (err) {
+            window.alert(`Error: ${err}`);
+          }
+        }
+      });
     } else {
-        window.alert("Cancelled!");
+      window.alert('Cancelled!');
     }
-}
+  }
 
-var formats = {
+  const formats = {
     GPX: 'gpx',
     KML: 'kml',
     XLSX: 'xlsx',
     FIT: 'fit',
     TCX: 'tcx',
     'All Formats': 'all',
-};
+  };
 
-setInterval(function() {
-    var toolsItem = $('a[data-action="addPlannedMove"]');
-    console.warn(toolsItem);
-    var sentinel = toolsItem.closest('ul').children('li.batchExporter');
+  const toolsItem = $(`
+<div id="batchExporter">
+  <ul class="menu--list menu--hover menu--right menu--padding">
+    <li>
+      <a class="link link--light middle-all"><i class="icon-210 icon-text size-12"></i><span>Tools</span></a>
+      <ul class="padding">
+        <li id="exportMove" data-toolbar-show="view" class="hidden-phone toolbar-button_export">
+          <ul id="exportList" class="grid--with-margins"></ul>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</div>`);
+
+  setInterval(function() {
+    const addMove = $('a[data-action="addPlannedMove"]');
+    const sentinel = addMove.closest('div').parent().children('div#batchExporter');
     if (!sentinel || sentinel.length === 0) {
-        _.forOwn(formats, function(format, name) {
-            var li = $('<li class="batchExporter"></li>');
-            var link = $('<a style="text-align: left;">Export selected as ' + name + '.</a>');
-            link.click(function() {
-                exportMoves(format);
-                return false;
-            });
-            li.append(link);
-            toolsItem.closest('li').before(li);
+      addMove.closest('div').parent().append(toolsItem);
+      _.forOwn(formats, (format, name) => {
+        const div = $('<li class="row-margin"></li>');
+        const link = $(`<a data-export-format="${format}" class="link exportMove">Export selected as ${name}.</a>`);
+        link.click(() => {
+          exportMoves(format);
+          return false;
         });
+        div.append(link);
+        toolsItem.find('ul#exportList').append(div);
+      });
     }
-}, 1000);
+  }, 1000);
 })();
